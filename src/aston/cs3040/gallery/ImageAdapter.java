97085fb9bcf3fbb06bb1b531fdef058ae91c7717
jay-g-patel <jay-g-patel@hotmail.co.uk>
2014-04-09 16:43:03 +0100

    get image from pictures directory

--- src/aston/cs3040/gallery/ImageAdapter.java
+++ src/aston/cs3040/gallery/ImageAdapter.java
@@ -1,7 +1,11 @@
 package aston.cs3040.gallery;
 
+import java.io.File;
+
 import android.widget.BaseAdapter;
 import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
@@ -12,13 +16,18 @@ public class ImageAdapter extends BaseAdapter
 {
 
 	private Context mContext;
-	 
+	
+	File f =  new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"Deleg8");
+	File[] mThumbIds = f.listFiles();
+	
+	
+	
     // Keep all Images in array
-    public Integer[] mThumbIds = {
-            R.drawable.ic_action_edit, R.drawable.ic_action_edit,
-            R.drawable.ic_action_new, R.drawable.ic_action_new,
-            R.drawable.ic_launcher, R.drawable.ic_launcher,
-    };
+//    public Integer[] mThumbIds = {
+//            R.drawable.ic_action_edit, R.drawable.ic_action_edit,
+//            R.drawable.ic_action_new, R.drawable.ic_action_new,
+//            R.drawable.ic_launcher, R.drawable.ic_launcher,
+//    };
  
     // Constructor
     public ImageAdapter(Context c){
@@ -48,7 +57,7 @@ public class ImageAdapter extends BaseAdapter
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView = new ImageView(mContext);
-        imageView.setImageResource(mThumbIds[position]);
+        imageView.setImageURI(Uri.fromFile(mThumbIds[position]));
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         imageView.setLayoutParams(new GridView.LayoutParams(70, 70));
         return imageView;
